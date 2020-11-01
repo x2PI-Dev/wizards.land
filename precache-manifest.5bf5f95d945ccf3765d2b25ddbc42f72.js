8027d6626a5654156b36bc1dda5dc6f1d4e96154
x2PI-Dev <73778817+x2PI-Dev@users.noreply.github.com>
2020-11-01 15:37:05 +0300

    Updates

---
+++ precache-manifest.5bf5f95d945ccf3765d2b25ddbc42f72.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "719a4fb38ec877e8a9b69ae206012bc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b1ab56561dcf29231296",
+    "url": "/static/css/2.86df01db.chunk.css"
+  },
+  {
+    "revision": "57446532e8708f303709",
+    "url": "/static/css/main.981f7211.chunk.css"
+  },
+  {
+    "revision": "b1ab56561dcf29231296",
+    "url": "/static/js/2.be9068b9.chunk.js"
+  },
+  {
+    "revision": "21ced859ea2b2d6b856d461ad6c2afed",
+    "url": "/static/js/2.be9068b9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "57446532e8708f303709",
+    "url": "/static/js/main.81b0f444.chunk.js"
+  },
+  {
+    "revision": "2aeadaa0fd525890ce84",
+    "url": "/static/js/runtime-main.1d8704a3.js"
+  },
+  {
+    "revision": "4e1ec8403d903dc514271d7328fbdeb3",
+    "url": "/static/media/persik.4e1ec840.png"
+  }
+]);
